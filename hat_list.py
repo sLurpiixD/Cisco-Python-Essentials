8adba819633cf4ae7f4c2efda9eef543eecb0ce6
sLurpiixD <67107811+sLurpiixD@users.noreply.github.com>
2024-09-14 15:12:19 +0800

    hat_list

---
+++ hat_list.py
@@ -0,0 +1,11 @@
+hat_list = [1, 2, 3, 4, 5]
+
+user_input = int(input("Input Number: "))
+middle_number = user_input
+hat_list[2] = middle_number
+
+
+del hat_list[4]
+
+print("\nList's length:", len(hat_list))
+print(hat_list)
